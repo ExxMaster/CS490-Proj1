f2ea6ea13578c439ab4d525f5df46435c0f46871
dolavesm <dolavesm@purdue.edu>
2015-01-29 18:47:24 -0500

    added commented TODO

--- SingleThreadedChatServer.java
+++ SingleThreadedChatServer.java
@@ -9,3 +9,20 @@ public static void main(String[] args){
 
 
 }
+/*
+while ( true ) {
+accept connection from client c ;
+receive message m from client c ;
+if ( m == register < name , ip , port >)
+// process registration , and send an a c k n o w l e d g e m e n t / failure to c
+else if (( m == heartbeat < name >)
+if ( previous heartbeat < name > was received less than
+hear tbeat_ra te seconds ago )
+// keep client c alive
+else
+// remove client c from G
+else if ( m == get )
+// send G to client c
+else
+// invalid message from client c , server closes c o n n e c t i o n ;
+}*/
