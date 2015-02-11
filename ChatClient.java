c629e94b79f44389f066e1f0a2e0259082b6e3cf
szfs <su68@purdue.edu>
2015-02-10 19:03:37 -0500

    chartclient

--- ChatClient.java
+++ ChatClient.java
@@ -1,30 +1,86 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
-//http://stackoverflow.com/questions/1436884/multithreaded-chat-server-in-java
-class ChatClient {
-  
-  static int heartbeat_rate = 5;
-    
-  public static void main(String[] args) throws IOException {
 
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Please Enter Your Name: ");
-    String _name = sc.nextLine();
-    String heartbeat = "anything";
-    String serverAddress = "data.cs.purdue.edu";
-    Socket s = null;
-    BufferedReader input = null;
-    PrintWriter out = null;
-    try{
-        s = new Socket(serverAddress, 5555);
-        input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        out = new PrintWriter(s.getOutputStream(), true);
-    }catch (IOException e) {
-      System.out.println("Cannection to server failed");
-      System.exit(1);
+class ChatClient {
+    
+    private String _name, _ip;
+    private int _port;
+    private Socket s;
+    
+    //constant variable
+    private static final int heartbeat_rate = 5;
+    private static final String serverAddress = "localhost";
+    private static final int portNumber = 1234;
+    
+    private ChatClient() {
+        //get the name
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please Enter Your Name: ");
+        this._name = sc.nextLine();
+        sc.close();
+        
+        try{
+            s = new Socket(serverAddress, portNumber);
+        }catch (IOException e) {
+            System.out.println("Cannection to server failed");
+            System.exit(1);
+        }
+        
+        this._ip = s.getLocalAddress().toString().substring(1);
+        this._port = s.getLocalPort();
+        
     }
-    //output to server
-    out.printf("<%s, %s, %d>\n", _name, s.getLocalAddress().toString(), s.getLocalPort());
-  }
+    
+    //register client
+    private boolean register() {
+        try {
+            //Send the register message to the server
+            OutputStream os = this.s.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+            
+            String m = "register<" + this._name + ", " + this._ip + ", " +  this._port + ">" + "\n";
+            System.out.println(m);
+            
+            bw.write(m);
+            bw.flush();
+            System.out.println("Message sent to the server : " + m);
+            
+        } catch (IOException e) {
+            System.out.println("error: register");
+        }
+        return true;
+    }
+    
+    private void sendHeartbeat() {
+        try {
+            while(true) {
+                if(this.s.isConnected()) {
+                    System.out.println("Still Alive");
+                } else {
+                    System.out.println("Dead Already");
+                }
+                OutputStream os = this.s.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os);
+                BufferedWriter bw = new BufferedWriter(osw);
+                
+                String m = "heartbeat<" + this._name + ", " + this._ip + ", " +  this._port + ">" + "\n";
+                System.out.println(m);
+                bw.write(m);
+                bw.flush();
+                System.out.println("Message sent to the server : " + m);
+                Thread.sleep(heartbeat_rate * 1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        ChatClient cc = new ChatClient();
+        cc.register();
+        cc.sendHeartbeat();
+    }
+    
 }
