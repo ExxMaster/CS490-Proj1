11a9668b0d873f8a45a2bcce8d9f9170ba7f0f1a
dolavesm <dolavesm@purdue.edu>
2015-02-22 16:14:53 -0500

    fixed errors and added readme

--- PA1/src/edu/purdue/cs/RMIaddTest/RMIChatServer.java
+++ PA1/src/edu/purdue/cs/RMIaddTest/RMIChatServer.java
@@ -4,9 +4,8 @@ import java.rmi.*;
 public class RMIChatServer {
   public static void main(String args[]) {
     try {
-      RMIImplementation RMIImplementation = new RMIImplementation();
-      Naming.rebind("RMIChatServer", RMIImplementation);
-      System.out.println("Server: The sum is: " + RMIInterface.add(50.0,50.0));
+      RMIImplementation impl = new RMIImplementation();
+      Naming.rebind("RMIChatServer", impl);
     }
     catch(Exception e) {
       System.out.println("Exception: " + e);
